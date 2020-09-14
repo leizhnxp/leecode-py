d92061347d9834ab03a68e4a44e2a15cec33be86
雷振华 <leizhnxp@gmail.com>
2020-09-14 13:50:31 +0800

    combination sum ii 这道题
    + [题目链接](https://leetcode-cn.com/problems/combination-sum-ii/submissions/)

--- medium/testcases/test_combination_sum_ii.py
+++ medium/testcases/test_combination_sum_ii.py
@@ -2,6 +2,17 @@ from medium.combination_sum_ii import combination_sum2
 
 
 class TestComBinationSum:
+
+    def test_error(self):
+        actual = combination_sum2([4,1,1,4,4,4,4,2,3,5], 10)
+        expect = [[1,1,3,5],[1,1,4,4],[1,2,3,4],[1,4,5],[2,3,5],[2,4,4]]
+        print(actual)
+        print(expect)
+        expect = [tuple(x) for x in expect]
+        actual = [tuple(x) for x in actual]
+
+        assert set(actual) == set(expect)     
+
     def test_10_1_2_7_6_1_5__8(self):
         actual = combination_sum2([10, 1, 2, 7, 6, 1, 5], 8)
         expect = [[1, 7], [1, 2, 5],

71a2770b1827e55d6640f77c62d249abccd7cc6a
zhenhua.lei <leizhenhua@arrail-dental.com>
2020-09-25 18:35:55 +0800

    练习pytest参数测试夹具撰写

--- medium/testcases/test_subset.py
+++ medium/testcases/test_subset.py
@@ -1,7 +1,43 @@
+import pytest
+
 from medium.subset import Solution
+
+param = [
+    ([1, 2, 3], [
+        [3],
+        [1],
+        [2],
+        [1, 2, 3],
+        [1, 3],
+        [2, 3],
+        [1, 2],
+        []
+    ]),
+    ([1, 1, 1], [
+        [1],
+        [1, 1, 1],
+        [1, 1],
+        []
+    ])
+]
+
+
+@pytest.fixture(params=param)
+def params(request):
+    return request.param
 
 
 class TestSubset:
+
+    def test_subset(self, params):
+        input_ = params[0]
+        expect = params[1]
+        actual = Solution.subsets(input_)
+        assert len(actual) == len(expect)
+        expect = [tuple(x) for x in expect]
+        actual = [tuple(x) for x in actual]
+        assert set(actual) == set(expect)
+
     def test_123(self):
         parameters = [1, 2, 3]
         expect = [
@@ -29,6 +65,7 @@ class TestSubset:
             []
         ]
         actual = Solution.subsets(parameters)
+        print(actual)
         assert len(actual) == len(expect)
         expect = [tuple(x) for x in expect]
         actual = [tuple(x) for x in actual]
